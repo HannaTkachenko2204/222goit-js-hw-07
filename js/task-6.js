542ad7d9665f8497e0f17d5b201904840bbf3320
Анна <anna.kuvakina91@gmail.com>
2024-03-06 16:24:56 +0200

    home-work

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  for (let i = 1; i <= amount; i++) {
+    const element = document.createElement('div');
+    console.log(element);
+    element.style.width = `${30 + i * 10}px`;
+    element.style.height = `${30 + i * 10}px`;
+    element.style.backgroundColor = getRandomHexColor();
+    boxesEl.append(element);
+    //console.log(boxesEl);
+  }
+}
+
+createBtn.addEventListener('click', () => {
+    const amount = Number(inputEl.value);
+    console.log(amount);
+    createBoxes(amount);
+});
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+}
+
+destroyBtn.addEventListener('click', () => destroyBoxes());
